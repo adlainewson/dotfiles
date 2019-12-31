6b8d81c823d40fd1a921fca686f0e8b68b16bf0d
Adlai Newson <adlai.newson@gmail.com>
2019-12-31 15:54:43 -0800

    Update paths, add zshrc and aliases

--- checkscript.sh
+++ checkscript.sh
@@ -12,11 +12,13 @@ function ask {
 }
 
 declare -A dfiles
-dfiles[i3]="i3/config $HOME/.i3/config"
+dfiles[i3]="i3/config $HOME/.config/i3/config"
 dfiles[polybar]="polybar/config $HOME/.config/polybar/config"
 dfiles[ranger_rc]="ranger/rc.conf $HOME/.config/ranger/rc.conf"
 dfiles[ranger_rifle]="ranger/rifle.conf $HOME/.config/ranger/rifle.conf"
 dfiles[bash]=".bashrc $HOME/.bashrc"
+dfiles[zsh]=".zshrc $HOME/.zshrc"
+dfiles[aliases]=".aliases $HOME/.aliases"
 dfiles[tmux]=".tmux.conf $HOME/.tmux.conf"
 dfiles[vim]=".vimrc  $HOME/.vimrc"
 dfiles[Xresources]=".Xresources $HOME/.Xresources"
